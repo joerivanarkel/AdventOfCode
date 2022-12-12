27d4568e8cb25c781c1f254c553e5eb891330feb
joerivanarkel <Joeri@vanarkel.net>
2022-12-12 19:33:34 +0100

    part 1

--- 10/src/main.rs
+++ 10/src/main.rs
@@ -1,43 +1,52 @@
-use std::fs;
+use std::{fs, ops::RemAssign};
 
 fn main() {
     // Get Input
     let file_path = "./src/input.txt";
     let contents = fs::read_to_string(file_path).expect("Something went wrong reading the file");
-    let contents = contents.split('\n');
+    let binding = contents.replace("\r", "");
+    let contents = binding.split('\n').collect::<Vec<&str>>();
 
-    let mut iteration = 1;
-    let mut x_value_list: Vec<i32> = Vec::new();
-    x_value_list.push(1);
+    // loop through the input adn track iteratrion and x value
+    let mut x_value = 1;
+    let mut iteration = 0;
+
+    let mut signal_strengths = vec![vec![0; 40]; 6];
 
     for line in contents {
         if line.contains("noop") {
             iteration += 1;
-            check_iteration(iteration, &mut x_value_list);
+            check_iteration(iteration, x_value)
         }
         if line.contains("addx") {
-            iteration += 2;
-            check_iteration(iteration, &mut x_value_list);
+            iteration += 1;
+            check_iteration(iteration, x_value);
+            iteration += 1;
+            check_iteration(iteration, x_value);
+            
 
             let split_line = line.split(' ').last().unwrap();
-            let x_value_to_add = split_line.parse::<i32>().unwrap();
-            x_value_list.push(x_value_to_add);
+            let x_value_to_add : i32 = split_line.parse().unwrap();
+            x_value += x_value_to_add;
         }
     }
+
+
+    
 }
 
-fn check_iteration(iteration: i32, x_value_list: &mut Vec<i32>) {
-    match iteration {
-        20 | 60 | 100 | 140 | 180 | 220 => {
-            println!("Iteration: {}", iteration);
+fn check_iteration(iteration: i32, x_value: i32) {
+    //check if cycle is between 1 and 40
+    if 1 <= iteration && iteration <= 40 {
+        println!("Iteration: {}", iteration);
+        
+        let signal_strength = x_value * iteration;
 
-            let x_value = x_value_list.iter().sum::<i32>();
-            let signal_strength = x_value * iteration;
-
-            println!("X Value: {}", x_value);
-            println!("Signal Strength: {}", signal_strength);
-            println!("");
-        }
-        i32::MIN..=19_i32 | 21_i32..=i32::MAX => {}
+        println!("X Value: {}", x_value);
+        println!("Signal Strength: {}", signal_strength);
+        println!("");
     }
-}
+
+    return 0;
+}j
+
